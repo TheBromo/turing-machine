8c4240f3bc8335280d51960bedf4bdecd73c9fbf
Manuel <manuel@strenge.ch>
2023-04-27 15:23:33 +0200

    finished tape

--- machine/tape.go
+++ machine/tape.go
@@ -1,1 +1,33 @@
 package machine
+
+import "errors"
+
+type Tape struct {
+	Content  []rune
+	Position int
+}
+
+const (
+	Right  = iota
+	Left   
+)
+
+func (t *Tape) Read() rune {
+	return t.Content[t.Position]
+}
+
+func (t *Tape) Move(direction rune, write rune) (rune, error) {
+
+	if t.Position >= 0 && t.Position < len(t.Content) {
+		t.Content[t.Position] = write
+
+		if direction == Right {
+			t.Position = t.Position - 1
+		} else {
+			t.Position = t.Position + 1
+		}
+		return t.Read(), nil
+	} else {
+		return rune(0), errors.New("position out of bounds")
+	}
+}
